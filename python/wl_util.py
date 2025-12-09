2c8031c03a2c4d502a282d81c2bfe7649078531f
Sujal Singh <email.sujalsingh@gmail.com>
2025-12-09 11:46:51 +0530

    implement reading interface from wayland.xml

--- python/wl_util.py
+++ python/wl_util.py
@@ -3,6 +3,7 @@ import struct
 from dataclasses import dataclass
 from io import BytesIO
 from typing import Dict, List, Optional
+from xml.etree import ElementTree
 
 
 class WLPrimitive:
@@ -138,3 +139,71 @@ class Message:
         payload = self.serialize_payload()
         self.header.size = 8 + len(payload)
         return self.header.serialize() + payload
+
+
+def build_interface(path="/usr/share/wayland/wayland.xml"):
+    xml = ElementTree.parse(path)
+
+    interface = {}
+    arg_map = {
+        "int": Int32,
+        "uint": UInt32,
+        "fixed": "fixed",
+        "object": ObjID,
+        "new_id": NewID,
+        "string": String,
+        "fd": "fd",
+        "array": "array",
+    }
+
+    for interface_tag in xml.getroot().iter("interface"):
+        name = interface_tag.attrib["name"]
+        interface_obj = WLInterface(
+            name,
+            int(interface_tag.attrib["version"]),
+            {}, []
+        )
+
+        for opcode, request_tag in enumerate(interface_tag.iter("request")):
+            request_obj = WLRequest(
+                opcode,
+                (req_name := request_tag.attrib["name"]),
+                [],
+                type_=request_tag.attrib.get("type", None),
+            )
+
+            for arg_tag in request_tag.iter("arg"):
+                # noinspection PyTypeChecker
+                arg_obj = WLArgument(
+                    arg_tag.attrib["name"],
+                    arg_map[arg_tag.attrib["type"]],
+                    arg_tag.attrib.get("interface", None)
+                )
+
+                request_obj.args.append(arg_obj)
+
+            interface_obj.requests[req_name] = request_obj
+
+        for opcode, event_tag in enumerate(interface_tag.iter("event")):
+            event_obj = WLEvent(
+                opcode,
+                event_tag.attrib["name"],
+                [],
+                type_=event_tag.attrib.get("type", None)
+            )
+
+            for arg_tag in event_tag.iter("arg"):
+                # noinspection PyTypeChecker
+                arg_obj = WLArgument(
+                    arg_tag.attrib["name"],
+                    arg_map[arg_tag.attrib["type"]],
+                    arg_tag.attrib.get("interface", None)
+                )
+
+                event_obj.args.append(arg_obj)
+
+            interface_obj.events.append(event_obj)
+
+        interface[name] = interface_obj
+
+    return interface
